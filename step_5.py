58e4474a4d141259c5da2d4a368803eef5e29c8f
mgeltov <mgeltov@mail.ru>
2023-01-02 22:16:39 +0300

    Доделал 5 задание

--- step_5.py
+++ step_5.py
@@ -10,9 +10,38 @@ def raw_actors(name1='Jack Black', name2='Dustin Hoffman'):
                 and netflix.cast like '%{name2}%'
         """
         cursor.execute(query)
-        raw_actors_list = []
+        raw_actors_list_1 = []
         for row in cursor.fetchall():
-            raw_actors_list.append(row)
+            raw_actors_list_1.append(row)
+
+        raw_actors_list = []
+        for item in raw_actors_list_1:
+            x = " ".join(item)
+            raw_actors_list.append(x)
+        raw_actors_list = str(raw_actors_list)
+        raw_actors_list = raw_actors_list.replace("[", "")
+        raw_actors_list = raw_actors_list.replace("]", "")
+        raw_actors_list = raw_actors_list.replace("'", "")
+        raw_actors_list = str.split(raw_actors_list, ", ")
+
         return raw_actors_list
 
-print(raw_actors())
+def counter_more_than_twice(name1='Jack Black', name2='Dustin Hoffman'):
+    actors = raw_actors(name1, name2)
+    counter = 1
+    actors_more_than_twice = []
+    for item in actors:
+        if item != name1 and item != name2:
+            if actors.count(item) > 2:
+                actors_dict = dict()
+                actors_dict['actor'] = item
+                actors_dict['counter'] = actors.count(item)
+                actors_more_than_twice.append(actors_dict)
+    unique_list = []
+    for item in actors_more_than_twice:
+        if item not in unique_list:
+            unique_list.append(item)
+
+    return unique_list
+
+print(counter_more_than_twice('Rose McIver',  'Ben Lamb'))
